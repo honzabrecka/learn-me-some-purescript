05eb9d0435d0fd86f20c84beeed5cc425b905b9a
Jan Brecka <jan.brecka@gmail.com>
2017-06-23 11:54:24 +0200

    pux

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,36 @@
 module Main where
 
-import Prelude
+import Prelude hiding (div)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Pux (CoreEffects, EffModel, start)
+import Pux.DOM.Events (onClick)
+import Pux.DOM.HTML (HTML)
+import Pux.Renderer.React (renderToDOM)
+import Text.Smolder.HTML (button, div, span)
+import Text.Smolder.Markup (text, (#!))
 
-main :: ∀ e. Eff (console :: CONSOLE | e) Unit
+data Event = Increment | Decrement
+
+type State = Int
+
+foldp :: ∀ fx. Event -> State -> EffModel State Event fx
+foldp Increment n = { state: n + 1, effects: [] }
+foldp Decrement n = { state: n - 1, effects: [] }
+
+view :: State -> HTML Event
+view count =
+  div do
+    button #! onClick (const Increment) $ text "Increment"
+    span $ text (show count)
+    button #! onClick (const Decrement) $ text "Decrement"
+
+main :: ∀ fx. Eff (CoreEffects fx) Unit
 main = do
-  log "Hello sailor!"
+  app <- start
+    { initialState: 0
+    , view
+    , foldp
+    , inputs: []
+    }
+
+  renderToDOM "#app" app.markup app.input
